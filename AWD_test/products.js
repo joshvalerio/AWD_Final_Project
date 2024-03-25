2184b375e9a182fa077b520b0fb298da53b1ad21
vinishmakaroonie <138175501+vinishmakaroonie@users.noreply.github.com>
2024-03-25 11:29:36 +0800

    Add files via upload

---
+++ AWD_test/products.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+        "id": 1,
+        "name": "600 PokéCoins",
+        "pro": "50 WEB STORE BONUS COINS",
+        "price": "149.00",
+        "image": "images/1.png",
+        "description": "550 + 50 Web Store Bonus = 600 total PokéCoins"
+    },
+    {
+        "id": 2,
+        "name": "1,300 PokéCoins",
+        "pro": "100 WEB STORE BONUS COINS",
+        "price": "289.00",
+        "image": "images/2.png",
+        "description": "1,200 + 100 Web Store Bonus = 1,300 total PokéCoins."
+    },
+    {
+        "id": 3,
+        "name": "2,700 PokéCoins",
+        "pro": "200 WEB STORE BONUS COINS",
+        "price": "589.00",
+        "image": "images/3.png",
+        "description": "2,500 + 200 Web Store Bonus = 2,700 total PokéCoins."
+    },
+    {
+        "id": 4,
+        "name": "5,600 PokéCoins",
+        "pro": "400 WEB STORE BONUS COINS",
+        "price": "1169.00",
+        "image": "images/4.png",
+        "description": "5,200 + 400 Web Store Bonus = 5,600 total PokéCoins."
+    },
+    {
+        "id": 5,
+        "name": "15,500 PokéCoins",
+        "pro": "1000 WEB STORE BONUS COINS",
+        "price": "2950.00",
+        "image": "images/5.png",
+        "description": "14,500 + 1,000 Web Store Bonus = 15,500 total PokéCoins."
+    }
+];
+
+export default products;
