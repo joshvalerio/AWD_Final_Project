6bbfea5a900b9c9652a857db543942d7d1b3f7c2
vinishmakaroonie <itsvincent49@gmail.com>
2024-03-28 17:37:14 +0800

    konti nalang

---
+++ payment.js
@@ -0,0 +1,4 @@
+    document.getElementById('next-btn').addEventListener('click', function() {
+        document.getElementById('billing-address').style.display = 'none';
+        document.getElementById('payment-method').style.display = 'block';
+    });
